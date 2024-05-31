f6c11c26a77d8b40a6998fdfc8586f41c8bc82a0
Ramon Olmos <ramon.olmos90@gmail.com>
2024-05-31 14:13:01 -0500

    ajuste a realizar

--- src/components/block/products.jsx
+++ src/components/block/products.jsx
@@ -24,7 +24,7 @@ const ProductList = ({clear_product, getCategory, clear_category, items, retriev
         setList([]);
         setHasMore(true);
         setLoading(false);
-        getMore(2);
+        getMore(1);
     }, [reset]);
 
     useEffect(() => {
